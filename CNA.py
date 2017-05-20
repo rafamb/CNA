545c53ad9bd9dc3b13aba16b725e511101b80270
rafamb <rfebarros@gmail.com>
2017-05-20 14:36:14 -0300

    Add Mat3

--- CNA.py
+++ CNA.py
@@ -22,10 +22,65 @@ def mat2(filename):
                                                 tasks_per_machine[m] += 1
                                                 mat2[i][m] += 1
                                                 mat2[m][i] += 1
-        return tasks_per_machine
+        return [mat2,tasks_per_machine]
+
+def mat3(matriz1,matriz2,tasks_per_machine):
+		ordemlinhas = []
+
+		#Primeira Linha
+
+		maior = 0
+		for i in xrange(1,len(tasks_per_machine)):
+			if tasks_per_machine[i] > tasks_per_machine[maior]:
+				maior = i
+		ordemlinhas.append(maior)
+
+		#Restante das Linhas
+		while(len(ordemlinhas)<len(tasks_per_machine)):
+			i = ordemlinhas[-1]
+			for j in xrange(len(tasks_per_machine)):
+				if j not in ordemlinhas:
+					maior = j
+					break
+			for j in xrange(len(tasks_per_machine)):
+				if j not in ordemlinhas:
+					if matriz2[i][j] > matriz2[i][maior]:
+						maior = j
+					elif matriz2[i][j] == matriz2[i][maior]:
+						if tasks_per_machine[j] > tasks_per_machine[maior]:
+							maior = j
+			ordemlinhas.append(maior)
+
+		#Construcao da nova matriz com as linhas reordenadas2
+		novaMat = []
+		for i in ordemlinhas:
+			novaMat.append(matriz1[i])
+
+		return [novaMat,ordemlinhas]
+
+
+
+
+
 
 if __name__ == "__main__":
-        filename = "matriz1.txt"
-        mat = mat2(filename)
-        for line in mat:
-                print line
+        filename = "matriz1.txt"	
+        matriz1 = mat1(filename)
+        print 'Matriz A'
+        for i in matriz1:
+        	print i
+        matriz2,tasks_per_machine = mat2(filename)
+        print
+        for i in xrange(len(tasks_per_machine)):
+        	matriz2[i].append('|')
+        	matriz2[i].append(tasks_per_machine[i])
+        print "Matriz B"
+        for i in matriz2:
+        	print i
+        print
+        nova,ordemlinhas = mat3(matriz1,matriz2,tasks_per_machine)
+        print ordemlinhas
+        print
+        print 'Matriz A com as linhas atualizadas'
+        for i in nova:
+        	print i
